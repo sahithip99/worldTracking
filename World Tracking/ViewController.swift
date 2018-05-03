39a1d50fd33e0651f1762d0689ce75f0e77d7f51
Tri Le <trile@Tris-Mac.local>
2018-05-03 15:53:35 -0400

    Added camera permission

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 import ARKit
 class ViewController: UIViewController {
     @IBOutlet weak var sceneView: ARSCNView!
-    let configuartion = ARWorldTrackingConfiguration()
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        self.sceneView.session.run(configuartion)
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
